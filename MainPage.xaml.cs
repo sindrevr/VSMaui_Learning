6eceac9550189ec62f1b643522dcbe3f65ab4562
sindrevr <sindre.vadla.ravnas@gmail.com>
2023-02-12 09:23:37 +0100

    test

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace Maui2023;
+
+public partial class MainPage : ContentPage
+{
+    bool ledbtnOnState = true;
+
+    public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    private void ledbtnclicked(object sender, EventArgs e)
+    {
+        if (ledbtnOnState == true)
+        {
+            ledbtn.Text = "LED OFF";
+            ledbtn.BackgroundColor = Colors.OrangeRed;
+            ledbtnOnState = false;
+          //  SemanticScreenReader.Announce(ledbtn.Text);
+            Console.WriteLine(ledbtnOnState);
+
+        }
+        else
+        {
+            ledbtn.Text = "LED ON";
+            ledbtn.BackgroundColor = Colors.Green;
+            ledbtnOnState = true;
+         //   SemanticScreenReader.Announce(ledbtn.Text);
+            Console.WriteLine(ledbtnOnState);
+        }
+            
+
+
+
+
+    }
+
+}
+
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui2023;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
